d82aebc854d935136caa06d7fe564c96fa536a26
eshoe2020 <hsulainey@gmail.com>
2021-03-15 22:30:53 -0600

    updates secret_key

--- project4/settings.py
+++ project4/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-# SECRET_KEY = os.getenv('SECRET')
-SECRET = 'ka_#*=p2nyyf3b)ke7(#wk^5zl_hadk)^+z%=97_3u8n-o#q)^'
+SECRET_KEY='ka_#*=p2nyyf3b)ke7(#wk^5zl_hadk)^+z%=97_3u8n-o#q)^'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
